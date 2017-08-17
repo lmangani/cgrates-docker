7033cf88cae61f87c6c5e5e64017317c68745324
Lorenzo <lorenzo.mangani@gmail.com>
2017-08-17 05:36:46 -0400

    add tutorials

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # cgrates-docker
-CGRates in Docker
+CGRates Docker Container embedded w/ mysql,redis
+
+
+## Options
+CGRateS comes with presets for the "big four" platforms. Use the ```TUTORIAL``` variable to define your platform of choice at execution time:
+
+### Dev (vanilla)
+```docker run -tid --name cgrates -p 2012:2012 qxip/cgrates-docker```
+
+### Asterisk
+```docker run -tid --name cgrates -e TUTORIAL='asterisk' -p 2012:2012 qxip/cgrates-docker```
+
+### FreeSWITCH
+```docker run -tid --name cgrates -e TUTORIAL='freeswitch' -p 2012:2012 qxip/cgrates-docker```
+
+### OpenSIPS
+```docker run -tid --name cgrates -e TUTORIAL='opensips' -p 2012:2012 qxip/cgrates-docker```
+
+### Kamailio
+```docker run -tid --name cgrates -e TUTORIAL='kamailio' -p 2012:2012 qxip/cgrates-docker```
+
+## Docs
+CGRateS and its Tutorials are documented in full detail at [http://cgrates.readthedocs.io/en/latest/tutorials.html](http://cgrates.readthedocs.io/en/latest/tutorials.html)
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,50 +2,69 @@
 # CGRateS Docker (default)
 # https://github.com/lmangani/cgrates-docker
 
-GOROOT=/root/go
-GOPATH=/root/code
+function set_defaults () {
+	sqluser=root
+	sqlpassword=GCRateS.org
+	GOROOT=/root/go
+	GOPATH=/root/code
+	PATH_MYSQL_CONFIG=/etc/mysql/my.cnf
+	DATADIR=/var/lib/mysql
+}
 
-sqluser=root
-sqlpassword=GCRateS.org
-
-PATH_MYSQL_CONFIG=/etc/mysql/my.cnf
-perl -p -i -e "s/sql_mode=NO_ENGINE_SUBSTITUTION,STRICT_TRANS_TABLES/sql_mode=NO_ENGINE_SUBSTITUTION/" $PATH_MYSQL_CONFIG
-sed '/\[mysqld\]/a max_connections = 1024\' -i $PATH_MYSQL_CONFIG
-
-DATADIR=/var/lib/mysql
-
-# Handy-dandy MySQL run function
 function MYSQL_RUN () {
   chown -R mysql:mysql "$DATADIR"
   echo 'Starting mysqld'
   /etc/init.d/mysql start
-  #echo 'Waiting for mysqld to come online'
   while [ ! -x /var/run/mysqld/mysqld.sock ]; do
       sleep 1
   done
 }
 
-echo "Initializing DBs... "
+echo "Initializing... "
+
+	# set defaults
+	set_defaults
+	perl -p -i -e "s/sql_mode=NO_ENGINE_SUBSTITUTION,STRICT_TRANS_TABLES/sql_mode=NO_ENGINE_SUBSTITUTION/" $PATH_MYSQL_CONFIG
+	sed '/\[mysqld\]/a max_connections = 1024\' -i $PATH_MYSQL_CONFIG
+
 	# mysql
 	chown -R mysql:mysql "$DATADIR"
 	mysqld --initialize-insecure=on --user=mysql --datadir="$DATADIR"
 	MYSQL_RUN
-	mysql -u root -e "SET PASSWORD = PASSWORD('$sqlpassword');" 
+	mysql -u root -e "SET PASSWORD = PASSWORD('$sqlpassword');"
 
 	# redis
 	sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf
 
-	sed -i 's/ENABLE=false/ENABLE=true/g' /etc/default/cgrates
+	# enable service
+	# sed -i 's/ENABLE=false/ENABLE=true/g' /etc/default/cgrates
 
-# start services
-service rsyslog start
-service redis-server start
+	# start services
+	service rsyslog start
+	service redis-server start
 
-# setup mysql
-cd /usr/share/cgrates/storage/mysql && ./setup_cgr_db.sh root CGRateS.org
-
-# load tariff plan data
-#cd /usr/share/cgrates/data/tariffplans/osips_training; cgr-loader
+	# setup mysql
+	cd /usr/share/cgrates/storage/mysql && ./setup_cgr_db.sh root CGRateS.org
 
 echo "Starting CGRateS.... "
-cgr-engine -config_dir /usr/share/cgrates/conf/samples/tutlocal/
+
+if [ ! -z "$TUTORIAL" ]; then
+	cgr-engine -config_dir /usr/share/cgrates/conf/samples/tutlocal/
+elif [ "$TUTORIAL" == "asterisk" ]; then
+	# Asterisk
+	echo "Asterisk Preset"
+	/usr/share/cgrates/tutorials/asterisk_ari/cgrates/etc/init.d/cgrates start
+elif [ "$TUTORIAL" == "freeswitch" ]; then
+	#FreeSwitch
+	echo "FreeSWITCH Preset"
+	/usr/share/cgrates/tutorials/fs_evsock/freeswitch/etc/init.d/freeswitch start
+elif [ "$TUTORIAL" == "kamailio" ]; then
+	#Kamailio
+	echo "Kamailio Preset"
+	/usr/share/cgrates/tutorials/kamevapi/kamailio/etc/init.d/kamailio start
+elif [ "$TUTORIAL" == "opensips" ]; then
+	#OpenSIPS
+	echo "OpenSIPS Preset"
+	/usr/share/cgrates/tutorials/osips_async/opensips/etc/init.d/opensips start
+fi
+
